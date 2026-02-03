ead774d7128d9f0d7ed4f0deafb9719675a10e25
Thakur-Rohit-chauhan <rc9193854683@gmail.com>
2026-02-03 18:43:46 +0530

    Moving functionalities added for player

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    p1 = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while (True):
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
         screen.fill("black")
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        #print(dt)
 
 if __name__ == "__main__":
     main()
